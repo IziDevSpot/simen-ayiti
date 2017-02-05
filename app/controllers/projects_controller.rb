df4de0dff7b2a63098cf566fc31826ad54341347
Ismael Chery <izidevspot@gmail.com>
2017-02-05 15:25:07 -0500

    Association user with projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,12 +10,12 @@ before_action :find_project, only: [:show, :edit, :update, :destroy]
     end
 
     def new
-@project= Project.new
+@project= current_user.docs.build
     end
 
 
     def create
-@project= Project.new(project_params)
+@project= current_user.docs.build(project_params)
 
 if @project.save
     redirect_to @project
@@ -29,18 +29,9 @@ if @project.save
     end
 
     def update
-        if @project.update(project_params)
-        redirect_to @project
-
-        else
-            render 'edit'
-        end
-    
     end
 
     def destroy
-    @project.destroy
-    redirect_to projects_path
     end
 
 private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201042107) do
+ActiveRecord::Schema.define(version: 20170205201730) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170201042107) do
     t.decimal  "amount"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
