358a2961a1cb3135c7d9e6a90c4cf8db3f8fea69
Ismael Chery <izidevspot@gmail.com>
2017-02-05 14:14:50 -0500

    add projects views and actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,18 +1,27 @@
 class ProjectsController < ApplicationController
-
+before_action :find_project, only: [:show, :edit, :update, :destroy]
+    
     def index
+ @projects = Project.all.order("created_at DESC")
     end
 
     def show
+     
     end
 
     def new
-
+@project= Project.new
     end
 
 
     def create
+@project= Project.new(project_params)
 
+if @project.save
+    redirect_to @project
+    else 
+        render 'new'
+    end
     end
 
 
@@ -28,9 +37,11 @@ class ProjectsController < ApplicationController
 private
 
 def find_project
+       @project= Project.find(params[:id])  
 end
 
 def project_params
+    params.require(:project).permit(:name, :description,:amount)
 
 end
 
